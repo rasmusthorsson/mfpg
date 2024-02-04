c5a580724dbd7f9e3f890061563d5ff04d1d057e
Rasmus Thorsson <rasmus.thorsson@outlook.com>
2024-02-04 18:07:56 +0100

    Rewrote greedysolver, added an exception for unsolvable graphs, added g++ clause in cmake file to avoid incompatability issues with latest version.

--- src/HandPosition.h
+++ src/HandPosition.h
@@ -3,22 +3,28 @@
 
 #include "SimplifiedNote.h"
 
-//Output for one layer, a sequence of HandPositions should realistically be the output
-//of the program.
-//TODO Rename, add more attributes? turn into a list?
-template <class InputTuple> class HandPosition {
+//A linked list 
+template <class InputTuple, class Output> class HandPosition {
 	private:
 		InputTuple state;
 		SimplifiedNote note;
+		//Points to the layerlist corresponding to this HandPosition
+		LayerList<InputTuple, Output> layer_list; 
 	public:
 		HandPosition() {}
-		HandPosition(InputTuple i) : state(i) {}
-		HandPosition(InputTuple i, SimplifiedNote n) : state(i), note(n) {}
+		HandPosition(InputTuple i, LayerList<InputTuple, Output> ls) 
+			: state(i), layer_list(ls) {}
+		HandPosition(InputTuple i, SimplifiedNote n, 
+				LayerList<InputTuple, Output> ls) 
+				: state(i), note(n), layer_list(ls) {}
 		InputTuple getState() {
 			return state;
 		}
 		SimplifiedNote getNote() {
 			return note;
+		}
+		LayerList<InputTuple, Output> getLayerList() {
+			return layer_list;
 		}
 }; 
 #endif
