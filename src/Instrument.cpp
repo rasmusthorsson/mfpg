254099bd9ba549fbe7f0e9197e5c168e7ee3fc7a
Rasmus Thorsson <rasmus.thorsson@outlook.com>
2024-03-01 19:00:21 +0100

    Changed typename to OutputViable for instrument implementation.

--- src/Instrument.cpp
+++ src/Instrument.cpp
@@ -1,25 +1,25 @@
 #include "Instrument.h"
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 Instrument<OutputValue>::Instrument(std::vector<IString> sv) : strings(sv) {};
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 Instrument<OutputValue>::Instrument(std::initializer_list<IString> list) {
 	for (IString s : list) {
 		strings.push_back(s);	
 	}
 }
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 Instrument<OutputValue>::Instrument(std::vector<IString> sv, std::shared_ptr<ActionSet<OutputValue>> as) : action_set(as), strings(sv) {}
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 Instrument<OutputValue>::Instrument(std::shared_ptr<ActionSet<OutputValue>> as) : action_set(as) {}
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 Instrument<OutputValue>::~Instrument() {}
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 int Instrument<OutputValue>::makeIString(int pos, noteenums::Note start, noteenums::Note end) {
 	if (pos < 1) {
 		return -1;
@@ -34,12 +34,12 @@ int Instrument<OutputValue>::makeIString(int pos, noteenums::Note start, noteenu
 }
 
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 const std::shared_ptr<ActionSet<OutputValue>> Instrument<OutputValue>::getActionSet() const {
 	return action_set;
 }
 
-template<typename OutputValue>
+template<OutputViable OutputValue>
 const std::vector<IString>& Instrument<OutputValue>::getIStrings() {
 	return strings;
 }
