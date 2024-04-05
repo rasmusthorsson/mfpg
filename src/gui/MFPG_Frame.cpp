1ab23ab8ca7be9d7e3ee0ce45b9a85a4ac8fcd47
Rasmus Thorsson <rasmus.thorsson@outlook.com>
2024-04-05 22:33:14 +0200

    Started constructing the GUI.

--- src/gui/MFPG_Frame.cpp
+++ src/gui/MFPG_Frame.cpp
@@ -3,20 +3,37 @@
 #include <wx/docview.h>
 #include <wx/textctrl.h>
 #include <wx/choicebk.h>
+#include <wx/valtext.h>
+#include <wx/statbox.h>
+#include <wx/combobox.h>
 
-MFPG_Frame::MFPG_Frame() : wxFrame(nullptr, wxID_ANY, "MFPG") {
+#define WIDTH 1280
+#define HEIGHT 900
+
+MFPG_Frame::MFPG_Frame() : wxFrame(nullptr, wxID_ANY, "MFPG", wxDefaultPosition, wxSize(WIDTH, HEIGHT),
+		wxDEFAULT_FRAME_STYLE, wxFrameNameStr) {
 	wxMenu *menuFile = new wxMenu;
 
-	menuFile->Append(ID_Score, "&Select Score","");
+	menuFile->Append(ID_MenuNewScore, "&Select Score","");
 	menuFile->AppendSeparator();
 	menuFile->Append(wxID_EXIT);
 
 	wxMenu *menuHelp = new wxMenu;
 	menuHelp->Append(wxID_ABOUT);
+	
+	wxMenu *menuConfig = new wxMenu;
+	menuConfig->Append(ID_MenuNewConfig, "&New Config");
+	menuConfig->AppendSeparator();
+	menuConfig->Append(ID_MenuSaveConfig, "&Save Config");
+	menuConfig->AppendSeparator();
+	menuConfig->Append(ID_MenuSaveAsConfig, "&Save Config As");
+	menuConfig->AppendSeparator();
+	menuConfig->Append(ID_MenuDeleteConfig, "&Delete Config");
 
 	wxMenuBar *menuBar = new wxMenuBar;
 	menuBar->Append(menuFile, "&File");
 	menuBar->Append(menuHelp, "&Help");
+	menuBar->Append(menuConfig, "&Configs");
 
 	SetMenuBar( menuBar );
 
@@ -25,39 +42,78 @@ MFPG_Frame::MFPG_Frame() : wxFrame(nullptr, wxID_ANY, "MFPG") {
 	status += VERSION_MFPG;
 	SetStatusText(status);
 	
-	wxChoicebook *book = new wxChoicebook(this, ID_Config);
-	wxPanel *panel = new wxPanel(book);
-	new wxButton( panel, ID_Config,
-  		_T("Button &1"), wxPoint(50,30), wxSize(100,30) );
+	config_book = new wxChoicebook(this, 1);
+	config_panel = new wxPanel(config_book);
+	wxStaticBox *box = new wxStaticBox(config_panel, wxID_ANY, "Note Mapper", wxPoint(10, 10),
+			wxSize(200, 200), 0, wxStaticBoxNameStr);
+	wxString choices[] = {"Basic", "CSV File"};
+	notemap_box = new wxComboBox(box, ID_CBNoteMapper, _T("Basic"), wxPoint(20, 20), 
+			wxSize(150, 40), 2, choices, wxCB_READONLY, wxDefaultValidator, wxComboBoxNameStr);
+
+	config_book->AddPage(config_panel, _T("Config1"), true);
 
 }
 
 wxBEGIN_EVENT_TABLE(MFPG_Frame, wxFrame)
-	EVT_MENU(wxID_EXIT, MFPG_Frame::OnExit)
-	EVT_MENU(wxID_ABOUT, MFPG_Frame::OnAbout)
-	EVT_MENU(ID_Score, MFPG_Frame::OnScore)
-	EVT_BUTTON(ID_Config, MFPG_Frame::OnConfig)
+	EVT_MENU(wxID_EXIT, MFPG_Frame::MenuExit)
+	EVT_MENU(wxID_ABOUT, MFPG_Frame::MenuAbout)
+	EVT_MENU(ID_MenuNewScore, MFPG_Frame::MenuNewScore)
+	EVT_MENU(ID_MenuNewConfig, MFPG_Frame::MenuNewConfig)
+	EVT_MENU(ID_MenuSaveConfig, MFPG_Frame::MenuSaveConfig)
+	EVT_MENU(ID_MenuSaveAsConfig, MFPG_Frame::MenuSaveAsConfig)
+	EVT_MENU(ID_MenuDeleteConfig, MFPG_Frame::MenuDeleteConfig)
+
+	EVT_COMBOBOX(ID_CBNoteMapper, MFPG_Frame::CBNoteMapper)
 wxEND_EVENT_TABLE()
 
-void MFPG_Frame::OnExit(wxCommandEvent& event) {
+void MFPG_Frame::MenuExit(wxCommandEvent& event) {
 	Close(true);
 }
 
-void MFPG_Frame::OnAbout(wxCommandEvent& event) {
+void MFPG_Frame::MenuAbout(wxCommandEvent& event) {
 	wxMessageBox("This is a wxWidgets Hello World example",
 		"About Hello World", wxOK | wxICON_INFORMATION);
 }
 
-void MFPG_Frame::OnScore(wxCommandEvent& event) {
+void MFPG_Frame::MenuNewScore(wxCommandEvent& event) {
 	wxFileDialog *file_dialog = new wxFileDialog(this, _("Choose a musicXML file to open"), 
 			wxEmptyString, wxEmptyString, _("XML files (*.xml)|*.xml"), wxFD_OPEN, 
 			wxDefaultPosition);
 	if (file_dialog->ShowModal() == wxID_OK) {
-		score = file_dialog->GetPath();
+		score_path = file_dialog->GetPath();
 	}
 }
-
-void MFPG_Frame::OnConfig(wxCommandEvent& event) {
+void MFPG_Frame::MenuNewConfig(wxCommandEvent& event) {
+	wxString new_conf;
+	wxTextEntryDialog *input_dialog = new wxTextEntryDialog(this, _("Config Name"), 
+			_("New Config"), wxEmptyString, wxTextEntryDialogStyle, wxDefaultPosition);
+	input_dialog->SetMaxLength(16);
+	wxTextValidator config_names(wxFILTER_INCLUDE_CHAR_LIST);
+	config_names.SetCharIncludes(config_chars);
+	input_dialog->SetTextValidator(config_names);
+	if (input_dialog->ShowModal() == wxID_OK) {
+		new_conf = input_dialog->GetValue();
+	}
+	config_book->AddPage(config_panel, new_conf, true);
+}
+void MFPG_Frame::MenuSaveConfig(wxCommandEvent& event) {
+		
+}
+void MFPG_Frame::MenuSaveAsConfig(wxCommandEvent& event) {
 	
 }
-
+void MFPG_Frame::MenuDeleteConfig(wxCommandEvent& event) {
+	
+}
+void MFPG_Frame::CBNoteMapper(wxCommandEvent& event) {
+	if (notemap_box->GetCurrentSelection() == 0) {
+		notemapper = Settings::NOTEMAPPER_BASIC;
+		wxMessageBox("BASIC");
+	} else if (notemap_box->GetCurrentSelection() == 1) {
+		notemapper = Settings::NOTEMAPPER_CSV;
+		wxMessageBox("CSV File");
+	} else {
+		notemapper = Settings::UNDEFINED;
+		wxMessageBox("UNDEFINED NOTEMAPPER");
+	}
+}
--- src/gui/MFPG_Frame.h
+++ src/gui/MFPG_Frame.h
@@ -2,21 +2,48 @@
 #define MFPG_FRAME_H
 
 #include "wx/wx.h"
+#include "wx/choicebk.h"
+#include "wx/combobox.h"
+#include "Instrument.h"
+#include "NoteMapper.h"
+#include "BasicNoteMapper.h"
+#include "CSVNoteMapper.h"
+#include "Gui_Settings.h"
+
+using Distance = int;
 
 class MFPG_Frame : public wxFrame {
 	private:
+		const wxString config_chars = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-");
 		DECLARE_EVENT_TABLE()
 	public:
-		void OnScore(wxCommandEvent& event);
-		void OnExit(wxCommandEvent& event);
-		void OnAbout(wxCommandEvent& event);
-		void OnConfig(wxCommandEvent& event);
-		wxString score;
 		MFPG_Frame();
+		void MenuNewScore(wxCommandEvent&);
+		void MenuNewConfig(wxCommandEvent&);
+		void MenuSaveConfig(wxCommandEvent&);
+		void MenuSaveAsConfig(wxCommandEvent&);
+		void MenuDeleteConfig(wxCommandEvent&);
+		void MenuExit(wxCommandEvent&);
+		void MenuAbout(wxCommandEvent&);
+		void CBNoteMapper(wxCommandEvent&);
+		
+		Settings notemapper = UNDEFINED;
+		Settings actionset = UNDEFINED;
+		Settings instrument = UNDEFINED;
+		Settings solver = UNDEFINED;
+
+		wxString score_path;
+		wxChoicebook *config_book;
+		wxPanel *config_panel;
+		wxComboBox *notemap_box;
 };
 
 enum {
-	ID_Score = 1,
-	ID_Config = 2
+	ID_MenuNewScore = 101,
+	ID_MenuNewConfig = 102,
+	ID_MenuSaveConfig = 103,
+	ID_MenuSaveAsConfig = 104,
+	ID_MenuDeleteConfig = 105,
+	ID_CBNoteMapper = 201,
 };
 #endif
